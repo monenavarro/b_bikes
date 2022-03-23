3da3e8e37594bd158992513574a14d166adf43d5
monenavarro <me.navarro.97@outlook.com>
2022-03-23 17:33:41 +0000

    Initialise to initialiZe

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -2,19 +2,23 @@ require_relative "bike"
 
 class DockingStation
   attr_reader :bikes
-
-  def initialise
+  attr_reader :bike
+ 
+  def initialize
     @bikes = []
   end
 
   def dock(bike)
     fail 'Dock already full' if @bikes.count >= 20
     @bikes << bike
+    @bike = bike
   end
 
+
   def release_bike
-    fail 'No bikes available' unless @bikes.empty?
+    fail 'No bikes available' if @bikes.empty?
     @bikes.pop
+    @bike
   end
 
 end
